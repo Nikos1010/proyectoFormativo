9cecc43489b3866e1bc7f6ab8401736d32a1866a
Nicolas Montealegre <nimoba101@gmail.com>
2021-12-05 20:50:56 -0500

    Agregando algunos puntos finales, falta una pagina

--- PruebaPsicotecnica/html/prueba_op5.php
+++ PruebaPsicotecnica/html/prueba_op5.php
@@ -18,39 +18,93 @@
 
 <body>
     <!-- Menu Navegacion -->
-    <?php include 'header.php' ?>
+    <?php include 'header.php';
+    $candidato_id;
+    $intento_id;
+    ?>
 
-    <!-- Llamar base de datos -->
+    <!-- Llamar base de datos
     <?php include_once "../util/coneccion.php";
     $sentencia = $bd->query("SELECT candidato.nombre, prueba.id_prueba, preguntas.numero, preguntas.pregunta, respuestas.respuesta FROM preguntas INNER JOIN prueba ON preguntas.id_prueba = prueba.id_prueba INNER JOIN respuestas ON preguntas.id_pregunta = respuestas.id_respuesta INNER JOIN intentos ON prueba.id_prueba = intentos.id_prueba INNER JOIN candidato ON intentos.id_candidato = candidato.id_candidato");
     $pregunta = $sentencia->fetchAll(PDO::FETCH_OBJ);
     //print_r($pregunta);
-    ?>
+    ?> -->
 
     <h1>Preguntas - Respuestas de un candidato</h1>
 
     <!-- Busqueda -->
     <div class="container">
-        <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Ingrese Identificacion" aria-label="Recipient's username" aria-describedby="button-addon2">
-            <button class="btn btn-outline-primary me-5" type="button" id="button-addon2">Buscar</button>
-            <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false"></button>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Esperando...</a></li>
-            </ul>
-            <input type="text" class="form-control" placeholder="Escoja Empresa" aria-label="Text input with dropdown button">
-
-        </div>
-    </div>
-    <!-- Nombre y Numero de prueba -->
-    <div class="container">
-        <fieldset disabled>
-            <div class="input-group mb-3">
-                <input type="text" class="form-control me-4" placeholder="Nombre Candidato">
-                <input type="text" class="form-control me-4" placeholder="N° de prueba">
+        <form class="row g-3 mb-3" method="POST" action="">
+            <!-- Pedir Identificacion -->
+            <div class="col-md-6 mb-2">
+                <input type="text" class="form-control " name="buscador" placeholder="Ingrese Identificacion" autofocus>
             </div>
-        </fieldset>
+
+            <!-- Nombre Candidato -->
+            <div class="col-md-6">
+                <!-- Consultar candidato -->
+                <?php
+                if (isset($_POST['buscar'])) {
+                    $busqueda = $_POST['buscador'];
+
+                    $consulta = $bd->query("SELECT * FROM candidato where  identificacion= '$busqueda'");
+
+                    while ($row = $consulta->fetch()) {
+                ?>
+                        <fieldset disabled>
+                            <div class="col-md-12 mb-3">
+                                <input type="hidden" name="codigo" value="<?php $candidato_id = $row['id_candidato']; ?>">
+                                <input type="text" class="form-control" id="inputNombre4" value="<?php echo $row['nombre']; ?>">
+                            </div>
+                        </fieldset>
+                <?php
+                    }
+                }
+                ?>
+            </div>
+
+            <div class="row">
+                <!-- Elegir Intento -->
+                <div class="col-md-3 ">
+                    <div class="dropdown">
+
+                        <select class="btn btn-secondary dropdown-toggle" name="cbx_i" id="cbx_i">
+
+                            <option value="0">Seleccionar Intento
+                            </option>
+                            <?php
+
+                            $consulta = $bd->query("SELECT id_intento FROM intentos INNER JOIN candidato ON intentos.id_candidato = candidato.id_candidato WHERE candidato.id_candidato= '$candidato_id'; ");
+                            while ($row = $consulta->fetch()) {
+                            ?>
+                                <option value="<?php echo $row['id_intento']; ?>"><?php echo $row['id_intento']; ?></option>
+                            <?php
+                            }
+                            ?>
+
+                            <?php if (isset($_POST['buscar'])) {
+                                $intento_id = $_REQUEST['cbx_i'];
+                            }
+
+                            ?>
+                        </select>
+                    </div>
+                </div>
+
+                <!-- Boton Busqueda -->
+                <div class="col-md-3">
+                    <div class="btn-group ">
+                        <input type="submit" class="btn btn-outline-primary" name="buscar" value="Consultar">
+                    </div>
+                </div>
+        </form>
     </div>
+    <?php
+    if (isset($_POST['buscar'])) {
+        $sentencia = $bd->query("SELECT numero, pregunta, respuestas.respuesta FROM preguntas INNER JOIN prueba ON preguntas.id_prueba = prueba.id_prueba INNER JOIN respuestas ON preguntas.id_pregunta = respuestas.id_respuesta INNER JOIN intentos ON prueba.id_prueba = intentos.id_prueba INNER JOIN candidato ON intentos.id_candidato = candidato.id_candidato WHERE candidato.id_candidato = '$candidato_id' AND intentos.id_intento = '$intento_id';");
+        $pregunta = $sentencia->fetchAll(PDO::FETCH_OBJ);
+    }
+    ?>
     <!-- Tabla -->
     <div class="container">
         <div class="table-responsive">
@@ -63,11 +117,19 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Sigue la secuencia 6, 1, 8, 3, 10, ?</td>
-                        <td>12</td>
-                    </tr>
+                    <?php
+                    if (isset($_POST['buscar'])) {
+                        foreach ($pregunta as $dato) {
+                    ?>
+                            <tr>
+                                <th scope="row"><?php echo $dato->numero; ?></th>
+                                <td><?php echo $dato->pregunta; ?></td>
+                                <td><?php echo $dato->respuesta; ?></td>
+                            </tr>
+                    <?php
+                        }
+                    }
+                    ?>
                 </tbody>
             </table>
         </div>
